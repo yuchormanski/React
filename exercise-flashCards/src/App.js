db50ebe12f5c70118959ede99a0f4d31d2d13c42
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-09-03 22:29:42 +0300

    up

---
+++ exercise-flashCards/src/App.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+import { questions } from "./questions.js";
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FlashCards />
+    </div>
+  );
+}
+
+function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
