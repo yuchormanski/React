c08c7e286ff34c5aab0574311022871e7711ec46
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-09-06 16:49:15 +0300

    up

--- exercise-accordion-component.v1/src/components/AccordionItem.js
+++ exercise-accordion-component.v1/src/components/AccordionItem.js
@@ -13,13 +13,7 @@ export default function AccordionItem({ num, title, text }) {
       <p className="number">{currentNum}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && (
-        <div className="content-box">
-          {text.map((el) => (
-            <p style={{ padding: "5px 0 0", textIndent: "10px" }}>{el}</p>
-          ))}
-        </div>
-      )}
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
